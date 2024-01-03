44bb041cdd1a56cea29a5355473c0cae4ed60e87
Shaikhanis7 <shaikhaniscoder07@gmail.com>
2024-01-03 11:43:29 +0530

    new commit

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-const http=require('http')
-const server=http.createServer((req,res)=>
+// const http=require('http')
+// const server=http.createServer((req,res)=>
+// {
+//     console.log(req.url)
+//     res.writeHead(200,{'cotent-type':'text/html'})
+//     res.write('<h1>Home Page<h1>')
+//     res.end()
+// })
+
+
+// server.listen(9000)
+
+
+
+// const express=require('express')
+// const app=express()
+// const path=require('path');
+
+// app.use(express.static('./public'))
+
+// app.all('*',(req,res)=>
+// {
+//     res.status(404).send('<h1>Resourse Not Found</h1>')
+// })
+// app.listen(5000,()=>
+// {
+//     console.log('server is listening on port 5000')
+// })
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
+
+
+const express=require('express')
+const app=express();
+
+app.get('/',(req,res)=>
 {
-    if(req.url==='/')
-    {
-        res.end('Welcome to our Home Page')
-    }
-    if(req.url==='/about')
-    {
-      res.end('Welcome to our short History')
-    }
-
-    res.end(
-        `<h1>Oops</h1>
-        <p> 404 Error</p>`
-    )
- 
+    res.json([{name:'john'},{name:'susan'}]);
 })
-
-server.listen(5000)
+app.listen(5000,()=>
+{
+    console.log('server is listening')
+})
